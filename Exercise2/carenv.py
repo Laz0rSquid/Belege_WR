1248fe82b51b36d6d0298936e3e25641a44c5839
Marcus Kopp <marcus.kopp86@gmail.com>
2017-11-25 20:33:53 +0100

    environment implemented

--- Exercise2/carenv.py
+++ Exercise2/carenv.py
@@ -1,30 +1,46 @@
+import numpy as np
+
 class JacksCarRentalEnvironment(object):
 
     def step(self, action):
-        """
-        Run one timestep of the environment's dynamics. When end of episode
-        is reached, reset() should be called to reset the environment's internal state.
-        Input
-        -----
-        action : an action provided by the environment
-        Outputs
-        -------
-        (observation, reward, done, info)
-        observation : agent's observation of the current environment
-        reward [Float] : amount of reward due to the previous action
-        done : a boolean, indicating whether the episode has ended
-        info : a dictionary containing other diagnostic information from the previous action
-        """
-        raise NotImplementedError
+        done = False
+        reward = 0
+        info = ''
+        
+        if(not done):
+            cars_requested_1 = np.random.poisson(3, 1)
+            cars_requested_2 = np.random.poisson(4, 1)
+            observation[0] = observation[0] - cars_requested_1
+            observation[1] = observation[1] - cars_requested_2
 
-    def reset(self):
-        """
-        Resets the state of the environment, returning an initial observation.
-        Outputs
-        -------
-        observation : the initial observation of the space. (Initial reward is assumed to be 0.)
-        """
-        raise NotImplementedError
+        if(np.min(observation) <= 0):
+            done = True
+            info = 'Not enought cars to fulfill requests!'
+
+        if (not done):
+            reward = (cars_requested_1 + cars_requested_2) * 10
+
+        if(not done):
+            if(observation[0] - action < 0 and observation[1] + action < 0):
+                done = True
+                info = 'Not enough cars to move!'
+            else:
+                observation[0] = observation[0] - action
+                observation[1] = observation[1] + action
+
+        if(not done):
+            cars_returned_1 = np.random.poisson(3, 1)
+            cars_returned_2 = np.random.poisson(2, 1)
+            observation[0] = observation[0] - cars_requested_1
+            observation[1] = observation[1] - cars_requested_2
+            np.clip(observation, None, 20)
+
+        return observation, reward, done, info
+
+    def reset(self, param=10):
+        global observation
+        observation = np.array([param, param])
+        return observation
 
     @property
     def action_space(self):
