d1dbc8bbb45b13eb5c691a8d80e70d5621cc4540
Marcus Kopp <marcus.kopp86@gmail.com>
2017-12-01 17:09:17 +0100

    initialization prototype

--- Exercise2/carenv.py
+++ Exercise2/carenv.py
@@ -2,6 +2,11 @@ import numpy as np
 
 class JacksCarRentalEnvironment(object):
 
+    MAX_CAPACITY = 20
+    TRANSFER_COST = 2
+    REQUEST_RATE = [3, 4]
+    RETURN_RATE = [3, 2]
+
     def step(self, action):
         done = False
         reward = 0
@@ -12,8 +17,8 @@ class JacksCarRentalEnvironment(object):
             info = 'No cars!'
         
         if(not done):
-            cars_requested_1 = np.random.poisson(3)
-            cars_requested_2 = np.random.poisson(4)
+            cars_requested_1 = np.random.poisson(REQUEST_RATE[0])
+            cars_requested_2 = np.random.poisson(REQUEST_RATE[1])
             observation[0] = observation[0] - cars_requested_1
             observation[1] = observation[1] - cars_requested_2
 
@@ -22,7 +27,7 @@ class JacksCarRentalEnvironment(object):
             info = 'Not enought cars to fulfill requests!'
 
         if (not done):
-            reward = (cars_requested_1 + cars_requested_2) * 10 - (abs(action) * 2)
+            reward = (cars_requested_1 + cars_requested_2) * 10 - (abs(action) * TRANSFER_COST)
         else:
             reward = -(abs(action) * 2)
             observation[0] = observation[0] - action
@@ -36,11 +41,11 @@ class JacksCarRentalEnvironment(object):
                 observation[0] = observation[0] - action
                 observation[1] = observation[1] + action
 
-        cars_returned_1 = np.random.poisson(3)
-        cars_returned_2 = np.random.poisson(2)
+        cars_returned_1 = np.random.poisson(RETURN_RATE[0])
+        cars_returned_2 = np.random.poisson(RETURN_RATE[1])
         observation[0] = observation[0] + cars_returned_1
         observation[1] = observation[1] + cars_returned_2
-        np.clip(observation, None, 20)
+        np.clip(observation, None, MAX_CAPACITY)
 
         return observation, reward, False, info
 
